ce6e4abdcbc083fc428ef7208ed7357a712bdc56
Roman Cardozo <Roman.alejandro.cardozo@gmail.com>
2022-07-14 11:06:15 -0300

    Beta 2

--- src/App.js
+++ src/App.js
@@ -1,44 +1,67 @@
-import React, { useRef, useEffect } from 'react';
-import { useLocation, Switch } from 'react-router-dom';
-import AppRoute from './utils/AppRoute';
-import ScrollReveal from './utils/ScrollReveal';
-import ReactGA from 'react-ga';
-
-// Layouts
-import LayoutDefault from './layouts/LayoutDefault';
-
-// Views 
-import Home from './views/Home';
-
-// Initialize Google Analytics
-ReactGA.initialize(process.env.REACT_APP_GA_CODE);
-
-const trackPage = page => {
-  ReactGA.set({ page });
-  ReactGA.pageview(page);
-};
-
-const App = () => {
-
-  const childRef = useRef();
-  let location = useLocation();
-
+import React, { useState, useEffect } from "react";
+import Clients from "./components/Clients";
+import Footer from "./components/Footer";
+import Free from "./components/Free";
+import Home from "./components/Home";
+import Like from "./components/Like";
+import Navbar from "./components/Navbar";
+import Release from "./components/Release";
+import ScrollToTop from "./components/ScrollToTop";
+import Signup from "./components/Signup";
+import SuperRare from "./components/SuperRare";
+import scrollreveal from "scrollreveal";
+import "./sass/index.scss";
+function App() {
+  const [theme, setTheme] = useState("dark");
+  const changeTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
   useEffect(() => {
-    const page = location.pathname;
-    document.body.classList.add('is-loaded')
-    childRef.current.init();
-    trackPage(page);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
-
+    const registerAnimations = () => {
+      const sr = scrollreveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(
+        `
+        nav,
+        .home,
+        .free,
+        .clients,
+        .super-rare,
+        .releases,
+        .like,
+        .signup,
+        footer
+    `,
+        {
+          interval: 500,
+        }
+      );
+    };
+    registerAnimations();
+  }, []);
+  window.setTimeout(() => {
+    const home = document.getElementsByClassName("home");
+    home[0].style.transform = "none";
+    const nav = document.getElementsByTagName("nav");
+    nav[0].style.transform = "none";
+  }, 1500);
   return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-        <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-        </Switch>
-      )} />
+    <div data-theme={theme} className="app-container">
+      <ScrollToTop />
+      <Navbar changeTheme={changeTheme} currentTheme={theme} />
+      <Home />
+      <Free />
+      <Clients />
+      <SuperRare />
+      <Release />
+      <Like />
+      <Signup />
+      <Footer />
+    </div>
   );
 }
 
